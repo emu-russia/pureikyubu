d5ef01c46b993695670977c9f3ac0d252e3ef28c
ogamespec <ogamespec@gmail.com>
2020-03-31 17:38:28 +0300

    RTC fix

--- SRC/Hardware/EI.cpp
+++ SRC/Hardware/EI.cpp
@@ -180,8 +180,7 @@ static void SRAMSave(SRAM *s)
 // use to get updated RTC
 void RTCUpdate()
 {
-    //exi.rtcVal = MAGIC_VALUE + (uint32_t)time(NULL);
-    exi.rtcVal = 0;
+    exi.rtcVal = (uint32_t)time(NULL) - MAGIC_VALUE;
 }
 
 //
