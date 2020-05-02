662d1ae02ecc875295441cd1a58f1b99c1aafa65
ogamespec <ogamespec@gmail.com>
2020-05-02 21:15:39 +0300

    Update Cache.h

--- SRC/Core/Cache.h
+++ SRC/Core/Cache.h
@@ -38,7 +38,7 @@ namespace Gekko
 
 		// You can disable cache emulation for debugging purposes.
 		// This does not apply to a locked cache.
-		bool DisableForDebugReasons = false;
+		bool DisableForDebugReasons = true;
 
 		uint8_t* LockedCache = nullptr;
 		uint32_t LockedCacheAddr = 0;
