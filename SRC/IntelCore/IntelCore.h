230ebc9c3776a14a4f14cee414453e07a1520566
ogamespec <ogamespec@gmail.com>
2021-04-06 18:08:24 +0300

    List of Intel instructions

--- SRC/IntelCore/IntelCore.h
+++ SRC/IntelCore/IntelCore.h
@@ -1,5 +1,9 @@
 // IntelCore public API
 
+// Only Intel will appear in the names and comments everywhere, sorry AMD.
+
+// Reference: https://software.intel.com/content/www/us/en/develop/articles/intel-sdm.html
+
 #pragma once
 
 #include "IntelAnalyzer.h"
