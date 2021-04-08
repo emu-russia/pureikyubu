f6492d28a8c85fe05fc0a9583fc14615f068149a
ogamespec <ogamespec@gmail.com>
2021-04-08 15:28:19 +0300

    IntelAssembler skeleton

--- SRC/IntelCore/IntelAssembler.cpp
+++ SRC/IntelCore/IntelAssembler.cpp
@@ -1,3 +1,48 @@
 // Intel instruction code generator.
 
 #include "pch.h"
+
+namespace IntelCore
+{
+
+	void IntelAssembler::Assemble16(AnalyzeInfo& info)
+	{
+
+	}
+
+	void IntelAssembler::Assemble32(AnalyzeInfo& info)
+	{
+
+	}
+
+	void IntelAssembler::Assemble64(AnalyzeInfo& info)
+	{
+
+	}
+
+#pragma region "Quick helpers"
+
+	template <> AnalyzeInfo& IntelAssembler::adc<16>()
+	{
+		AnalyzeInfo info;
+		Assemble16(info);
+		return info;
+	}
+
+	template <> AnalyzeInfo& IntelAssembler::adc<32>()
+	{
+		AnalyzeInfo info;
+		Assemble32(info);
+		return info;
+	}
+
+	template <> AnalyzeInfo& IntelAssembler::adc<64>()
+	{
+		AnalyzeInfo info;
+		Assemble64(info);
+		return info;
+	}
+
+#pragma endregion "Quick helpers"
+
+}
--- SRC/IntelCore/IntelAssembler.h
+++ SRC/IntelCore/IntelAssembler.h
@@ -4,5 +4,21 @@
 
 namespace IntelCore
 {
+	class IntelAssembler
+	{
+
+	public:
+
+		// Base methods
+
+		static void Assemble16(AnalyzeInfo& info);
+		static void Assemble32(AnalyzeInfo& info);
+		static void Assemble64(AnalyzeInfo& info);
+
+		// Quick helpers
+
+		template <size_t n> static AnalyzeInfo& adc();
+
+	};
 
 }
