b31bebbe426f2c5e3e22fca97ab74e64b717779a
ogamespec <ogamespec@gmail.com>
2021-05-27 11:25:39 +0300

    Macro for enabling a simple MMU

--- SRC/GekkoCore/Config.h
+++ SRC/GekkoCore/Config.h
@@ -7,3 +7,7 @@
 #ifndef GEKKOCORE_USE_JITC
 #define GEKKOCORE_USE_JITC 0	//!< Use the recompiler during main code execution (in runtime). Debugging (step-by-step execution) is always done using the interpreter.
 #endif
+
+#ifndef GEKKOCORE_SIMPLE_MMU
+#define GEKKOCORE_SIMPLE_MMU 0	//!< Use the simple MMU translation used in Dolphin OS (until games start using ARAM mapping, also not suitable for GC-Linux).
+#endif
