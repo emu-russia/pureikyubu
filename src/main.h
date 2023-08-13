2588dbea29e4bec04712211336f07efd4faf1d57
ogamespec <ogamespec@gmail.com>
2023-08-13 18:23:16 +0300

    wiki\main.md

--- src/main.h
+++ src/main.h
@@ -1,39 +1,4 @@
-/*
-
-# main
-
-This component is used to control host emulation (GameCube).
-
-You can assume that the "Loaded" state is equivalent to the fact that the GameCube is on (powered),
-and the "Running" state is equivalent to the main Gekko clock is running.
-
-The architecture of the emulator is designed in such a way that the running Gekko thread is the main driving force of the emulator.
-All other emulation threads are based on the Gekko emulated timer (Time Base Register).
-
-Thus, if the Gekko thread is in a suspended state, all other hardware modules are also "sleeping".
-
-## Supported file formats
-
-Supported formats:
-- DOL
-- ELF
-- GCM/ISO: unencrypted GameCube disk images
-
-## HWConfig
-
-To avoid using configuration from the HW component, all Flipper hardware emulation settings are aggregated
-into the HWConfig structure.
-
-## Jdi
-
-Jey-Dai interface allows you to control host emulation, load and unload executable files (DOL, ELF) or DVD images and so on.
-
-A list of commands can be found in EmuJdi.json
-
-*/
-
-// Emulator includes
-
+// Emulator main deck control
 #pragma once
 
 #define EMU_VERSION L"1.5"
--- wiki/Readme.md
+++ wiki/Readme.md
@@ -2,6 +2,14 @@
 
 This wiki contains information about the emulator for developers and anyone interested.
 
+The wiki does not contain any detailed descriptions of the GameCube architecture, but is more focused on describing the emulator's internals.
+
+But here are some diagrams, just as a memo.
+
+GameCube:
+
 ![gc_diagram](/imgstore/emu/gc_diagram.png)
+
+Flipper:
 
 ![Flipper_ASIC_Block_Diagram](/imgstore/emu/Flipper_ASIC_Block_Diagram.png)
