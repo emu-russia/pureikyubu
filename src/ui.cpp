d2f11db931fd7ab71043215997be4e880358bfb5
org <ogamespec@gmail.com>
2024-04-20 16:39:47 +0300

    WinMain

--- src/ui.cpp
+++ src/ui.cpp
@@ -670,7 +670,11 @@ char* FixCommandLine(char* lpCmdLine)
 /* Keyboard accelerators (no need to be shared). */
 HACCEL  hAccel;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(nShowCmd);
